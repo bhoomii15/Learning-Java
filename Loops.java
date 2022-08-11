4fd33b58b740e12925bc540a66e02f90a71ae7e7
Bhoomi <bhoomis1020@gmail.com>
2022-08-11 23:44:46 +0530

    added a file

---
+++ Loops.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Loops {
+    public static void main(String[] args) {
+        //while loop
+        int i =1;
+        while (i<=3){
+            System.out.println(i);
+            i++;
+            System.out.println("Loop execution is finished");
+        }
+       int j = 100;
+        while (j<=200){
+            System.out.println(j);
+            j++;
+        }
+
+
+
+    }
+}
